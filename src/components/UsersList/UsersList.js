011b7cb692b0decbabde851fed86f3828f2f2739
digitalbee <digitalbee.gg@gmail.com>
2023-09-08 09:51:06 +0200

    commit

---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import users from 'data/users'
+
+const UsersList = () => (
+       <div>
+        <ul>
+          {users.map(({ name, average, attendance }) => (
+          <li>
+              <div>{average}</div>
+              <div>
+                     <p>{name}</p>
+                     <p>{attendance}</p>
+              </div>
+              <div>
+                     <button>X</button>
+              </div>
+          </li>
+          ))}
+        </ul>
+       </div>
+);
+
+
+UsersList.proptypes = {};
+
+export default UsersList;
---
+++ src/data/users.js
@@ -0,0 +1,14 @@
+export const users = [
+       {
+              name: 'Adam Romanski',
+              attendance: '105%',
+              average: '5.0',
+
+       }, 
+       {
+              name: 'Gracjan Giecewicz',
+              attendance: '10%',
+              average: '6.0',
+
+       }
+]
